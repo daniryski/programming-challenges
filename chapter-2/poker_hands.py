1b3d774c67d8364f0f4cf58be1b826c2d8be5f7c
Dani Ryśki <dani.ryski@pm.me>
2021-11-27 16:12:06 +0200

    Solve Poker Hands (2.8.2)

---
+++ chapter-2/poker_hands.py
@@ -0,0 +1,77 @@
+"""2.8.2 Poker Hands"""
+
+import itertools
+
+
+hand_types = [
+        'high card', 'pair', 'two pairs', 'three cards',
+        'straight', 'flush', 'full house', 'four cards', 'straight flush']
+
+
+def rank_hand(hand):
+    hand_ranks = sorted('23456789TJQKA'.index(rank) for rank, _ in hand)
+
+    hand_type = 'high card'
+    hand_rank = tuple(reversed(hand_ranks))
+
+    for rank, group in itertools.groupby(hand_ranks):
+        count = len(list(group))
+
+        if count == 2:
+            if hand_type == 'high card':
+                hand_type = 'pair'
+                hand_rank = rank, *(r for r in hand_rank if r != rank)
+
+            elif hand_type == 'pair':
+                hand_type = 'two pairs'
+                hand_rank = rank, *(r for r in hand_rank if r != rank)
+
+            elif hand_type == 'three cards':
+                hand_type = 'full house'
+
+        elif count == 3:
+            if hand_type == 'high card':
+                hand_type = 'three cards'
+                hand_rank = (rank,)
+
+            elif hand_type == 'pair':
+                hand_type = 'full house'
+                hand_rank = (rank,)
+
+        elif count == 4:
+            hand_type = 'four cards'
+            hand_rank = (rank,)
+
+    is_straight = hand_ranks == list(range(hand_ranks[0], hand_ranks[0] + 5))
+    is_flush = len({ suit for _, suit in hand }) == 1
+
+    if is_straight and is_flush:
+        hand_type = 'straight flush'
+
+    elif is_straight:
+        hand_type = 'straight'
+
+    elif is_flush:
+        hand_type = 'flush'
+
+    return (hand_types.index(hand_type), hand_rank)
+
+
+while True:
+    try:
+        deal = input().split(maxsplit=9)
+
+    except EOFError:
+        break
+
+    black_rank = rank_hand(deal[:5])
+    white_rank = rank_hand(deal[5:])
+
+    if black_rank > white_rank:
+        print('Black wins.')
+
+    elif white_rank > black_rank:
+        print('White wins.')
+
+    else:
+        print("Tie.")
